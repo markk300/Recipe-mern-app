add983c1758e34bde23ce00eaee5965080cd6f4f
iammark <mrdamarkomm@gmail.com>
2023-06-25 18:58:58 +0200

    added recipe render url

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -12,7 +12,7 @@ export const Home = () => {
   useEffect(() => {
     const fetchRecipe = async () => {
       try {
-        const response = await axios.get("http://localhost:3001/recipes");
+        const response = await axios.get("https://recipe-mern-app-dnfk.onrender.com/recipes");
         setRecipes(response.data);
       } catch (err) {
         console.error(err);
